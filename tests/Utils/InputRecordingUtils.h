eba620f4b7a98fc3d974caef6c6172a2e94f1e0d
MichaelBCG <example@example.com>
2024-04-11 05:06:27 +0200

    Add default initialization to USTRUCT primitives in InputRecordingUtils.h

--- tests/Utils/InputRecordingUtils.h
+++ tests/Utils/InputRecordingUtils.h
@@ -5,56 +5,68 @@
 #include "CoreMinimal.h"
 #include "InputRecordingUtils.generated.h"
 
+/**
+ * @brief Struct representing axis data.
+ */
 USTRUCT()
 struct FAxisData
 {
     GENERATED_BODY()
 
     UPROPERTY()
-    FName Name;
+    FName Name; /**< The name of the axis. */
 
     UPROPERTY()
-    float Value;
+    float Value = 0.0f; /**< The value of the axis. */
 };
 
+/**
+ * @brief Struct representing action data.
+ */
 USTRUCT()
 struct FActionData
 {
     GENERATED_BODY()
 
     UPROPERTY()
-    FName Name;
+    FName Name; /**< The name of the action. */
 
     UPROPERTY()
-    FKey Key;
+    FKey Key; /**< The key associated with the action. */
 
     UPROPERTY()
-    bool State;
+    bool State = false; /**< The state of the action. */
 };
 
+/**
+ * @brief Struct representing input bindings data.
+ */
 USTRUCT()
 struct FBindingsData
 {
     GENERATED_BODY()
 
     UPROPERTY()
-    TArray<FAxisData> AxisValues;
+    TArray<FAxisData> AxisValues; /**< The array of axis values. */
 
     UPROPERTY()
-    TArray<FActionData> ActionValues;
+    TArray<FActionData> ActionValues; /**< The array of action values. */
 
     UPROPERTY()
-    float WorldTime{0.0f};
+    float WorldTime = 0.0f; /**< The world time. */
 };
 
+/**
+ * @brief Struct representing input data.
+ */
 USTRUCT()
 struct FInputData
 {
     GENERATED_BODY()
 
     UPROPERTY()
-    TArray<FBindingsData> Bindings;
+    TArray<FBindingsData> Bindings; /**< The array of input bindings data. */
 
     UPROPERTY()
-    FTransform InitialTransform;
+    FTransform InitialTransform; /**< The initial transform. */
 };
